05d7b44ffa84bbbe13d94ebb7ab655cace0879b2
Danilo Bronze <74279443+DaniloBronze@users.noreply.github.com>
2023-08-17 14:06:26 -0300

    Update AddUserController.php

--- src/Controller/AddUserController.php
+++ src/Controller/AddUserController.php
@@ -14,24 +14,22 @@ class AddUserController implements Controller
   public function processaRequisicao(): void
   {
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['acao'])) {
-      $nome = filter_input(INPUT_POST, 'nome');
+      $nome = trim(filter_input(INPUT_POST, 'nome'));
       $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
       $senha = filter_input(INPUT_POST, 'senha');
       $perfil = filter_input(INPUT_POST, 'perfil', FILTER_VALIDATE_INT);
 
-      // Verificar se todos os campos estão preenchidos
       if (empty($nome) || empty($email) || empty($senha) || $perfil === false) {
         echo '<script>alert("Por favor, preencha todos os campos corretamente.");</script>';
         echo '<script>window.location.href = "/painel";</script>';
       } else {
-        // Criar uma nova instância de Academia com os dados do formulário
+
+        $nome = htmlentities($nome, ENT_QUOTES, 'UTF-8');
         $user = new Academia($nome, $email, $senha, $perfil);
 
-        // Tenta adicionar o usuário
         if ($this->academiaRepository->add($user)) {
-          // Usar JavaScript para exibir a mensagem em um alert e redirecionar
           echo '<script>alert("Usuário cadastrado com sucesso!");';
-          header('Location: /painel');
+          echo '<script>window.location.href = "/painel";</script>';
         }
       }
     }
