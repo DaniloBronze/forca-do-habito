f40b9b499ca8327fc852295cf5889e3bae3aa8da
Danilo Bronze <74279443+DaniloBronze@users.noreply.github.com>
2023-08-17 13:51:14 -0300

    Update Academia.php

--- src/Entity/Academia.php
+++ src/Entity/Academia.php
@@ -7,13 +7,17 @@ namespace Dev\Academia\Entity;
 class Academia
 {
   public readonly int $id;
+  public readonly string $nome;
+  public readonly string $email;
+  public readonly string $senha;
+  public readonly string $perfil; // Agora definido como string
 
-  public function __construct(
-    public readonly string $nome,
-    public readonly string $email,
-    public readonly string $senha,
-    public readonly string $perfil,
-  ) {
+  public function __construct(string $nome, string $email, string $senha, string $perfil) // Alterado o tipo do argumento
+  {
+    $this->nome = $nome;
+    $this->email = $email;
+    $this->senha = $senha;
+    $this->perfil = $perfil;
   }
 
   public function setId(int $id): void
